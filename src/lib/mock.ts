1872613fbe9c4f9bf0670dd64734a3bca0b0f9c0
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-31 18:00:53 +0300

    add mock data

---
+++ src/lib/mock.ts
@@ -0,0 +1,51 @@
+export const mockData = [
+    {
+        "id": 1,
+        "title": "Junior Frontend Developer",
+        "salary": "до 80 000 ₽",
+        "location": "Ярославль / Удалённо",
+        "experience": "1–3 года"
+    },
+    {
+        "id": 2,
+        "title": "Middle React Developer",
+        "salary": "от 120 000 ₽",
+        "location": "Москва",
+        "experience": "3–5 лет"
+    },
+        {
+        "id": 3,
+        "title": "Senior Frontend Developer",
+        "salary": "от 300 000 ₽",
+        "location": "Санкт-Петербург / Удалённо",
+        "experience": "от 5 лет"
+    },
+    {
+        "id": 4,
+        "title": "Middle NextJS Developer",
+        "salary": "от 120 000 ₽",
+        "location": "Нижний Новгород",
+        "experience": "2-3 года"
+    },
+        {
+        "id": 5,
+        "title": "Junior Backend Developer",
+        "salary": "до 100 000 ₽",
+        "location": "Иркутск / Удалённо",
+        "experience": "1–3 года"
+    },
+    {
+        "id": 6,
+        "title": "Junior Frontend Developer",
+        "salary": "от 30 000 ₽",
+        "location": "Вязники",
+        "experience": "без опыта"
+    },
+        {
+        "id": 7,
+        "title": "Senior Backend Developer",
+        "salary": "от 320 000 ₽",
+        "location": "Москва / Удалённо",
+        "experience": "от 4 лет"
+    },
+]
